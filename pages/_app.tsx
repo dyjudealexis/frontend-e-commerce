301f63e4b4e27ced704ba3cdcea448d2f1dba3da
Jude Alexis Dy <dyjudealexis@gmail.com>
2025-06-22 14:57:18 +0800

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,37 +7,37 @@ import "@/styles/css/style.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import "@/styles/globals.css";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import FullPageSpinner from "@/components/Others/FullPageSpinner";
+// import { useRouter } from "next/router";
+// import { useEffect, useState } from "react";
+// import FullPageSpinner from "@/components/Others/FullPageSpinner";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const [isRouteLoading, setRouteLoading] = useState(false);
-  useEffect(() => {
-    const handleStart = () => setRouteLoading(true);
-    const handleComplete = () => setRouteLoading(false);
+  // const router = useRouter();
+  // const [isRouteLoading, setRouteLoading] = useState(false);
+  // useEffect(() => {
+  //   const handleStart = () => setRouteLoading(true);
+  //   const handleComplete = () => setRouteLoading(false);
 
-    // Use const tuples for events
-    const startArgs = ['routeChangeStart', handleStart] as const;
-    const completeArgs = ['routeChangeComplete', handleComplete] as const;
-    const errorArgs = ['routeChangeError', handleComplete] as const;
+  //   // Use const tuples for events
+  //   const startArgs = ['routeChangeStart', handleStart] as const;
+  //   const completeArgs = ['routeChangeComplete', handleComplete] as const;
+  //   const errorArgs = ['routeChangeError', handleComplete] as const;
 
-    router.events.on(...startArgs);
-    router.events.on(...completeArgs);
-    router.events.on(...errorArgs);
+  //   router.events.on(...startArgs);
+  //   router.events.on(...completeArgs);
+  //   router.events.on(...errorArgs);
 
-    return () => {
-      router.events.off(...startArgs);
-      router.events.off(...completeArgs);
-      router.events.off(...errorArgs);
-    };
-  }, [router.events]);
+  //   return () => {
+  //     router.events.off(...startArgs);
+  //     router.events.off(...completeArgs);
+  //     router.events.off(...errorArgs);
+  //   };
+  // }, [router.events]);
 
 
   return (
     <>
-      {isRouteLoading && <FullPageSpinner />}
+      {/* {isRouteLoading && <FullPageSpinner />} */}
       <Toaster position="bottom-right" reverseOrder={false} />
       <Component {...pageProps} />
     </>
